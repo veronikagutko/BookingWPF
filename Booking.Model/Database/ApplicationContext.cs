7a16303041c059567088757e349d1a76a90236b0
User <User@LAPTOP-V7GDS8QM>
2022-12-12 22:25:51 +0300

    Добавьте файлы проекта.

---
+++ Booking.Model/Database/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Booking.Model.Models;
+using Microsoft.EntityFrameworkCore;
+//using System.Data.Entity;
+using System.Xml.Schema;
+
+
+namespace Booking.Model.Database
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Phone;Integrated Security=True;Pooling=False");
+        }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
